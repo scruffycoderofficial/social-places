93084ef1fcb697a56a1e1efb9fb383233a03ecba
Luyanda Siko <sikoluyanda@gmail.com>
2022-05-20 00:16:32 +0200

    Created a  Contact's portfolio template

---
+++ src/Controller/Site/Portfolio/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Site\Portfolio;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/portfolio")
+     */
+    public function index(): Response
+    {
+        return $this->render('site/portfolio/index.html.twig');
+    }
+}
